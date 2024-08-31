84cd0b43bfbcf2f39e467dc60f5fde5e12e776fa
Aniket Potdar <aniketpanditpotdar@gmail.com>
2024-08-31 23:00:27 +0530

    changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,29 +4,29 @@ import styled from 'styled-components'
 import Logo from '../assets/logo.svg'
 
 const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("form")
+  event.preventDefault();
+  alert("form")
 }
 
 const Register = () => {
-    return (
-        <>
-            <FormContainer>
-                <form onSubmit={(event) => handleSubmit(event)}>
-                    <div className='brand'>
-                        <img src={Logo} alt="" />
-                        <h1>Chatify</h1>
-                    </div>
-                    <input type="text" placeholder='Username' name='username' onChange={(e) => handleChange(e)} />
-                    <input type="email" placeholder='Email' name='email' onChange={(e) => handleChange(e)} />
-                    <input type="password" placeholder='Password' name='password' onChange={(e) => handleChange(e)} />
-                    <input type="password" placeholder='Confirm Password' name='confirmPassword' onChange={(e) => handleChange(e)} />
-                    <button type='submit'>Create User</button>
-                    <span>alraedy have a account? <Link to={'/login'}>Login</Link></span>
-                </form>
-            </FormContainer>
-        </>
-    )
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className='brand'>
+            <img src={Logo} alt="" />
+            <h1>Chatify</h1>
+          </div>
+          <input type="text" placeholder='Username' name='username' onChange={(e) => handleChange(e)} />
+          <input type="email" placeholder='Email' name='email' onChange={(e) => handleChange(e)} />
+          <input type="password" placeholder='Password' name='password' onChange={(e) => handleChange(e)} />
+          <input type="password" placeholder='Confirm Password' name='confirmPassword' onChange={(e) => handleChange(e)} />
+          <button type='submit'>Create User</button>
+          <span>alraedy have a account? <a><Link to={'/login'}>Login</Link></a></span>
+        </form>
+      </FormContainer>
+    </>
+  )
 }
 
 const FormContainer = styled.div`
